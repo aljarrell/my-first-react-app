672c5e1e707b4b2bc66d31f981fee0d694702512
aljarrell <alan.k.jarrell@gmail.com>
2018-12-19 13:28:33 -0500

    first react app start

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    currentNumber : 210,
+    currentUser : "Alan"
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <h1>Alan</h1>
+            {this.state.currentNumber}
+          <p>
+            Hello World.
+          </p>
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class MyFirstComponent extends Component {
+  constructor() {
+    super()
+      state = {
+       currentNumber : 210,
+       currentUser : "Alan"
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <h1>Alan</h1>
+            {this.state.currentNumber}
+          <p>
+            Hello World.
+          </p>
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default MyFirstComponent;
